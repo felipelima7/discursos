378f1cb1d50b3abf839d124fa8b2a48cae58c819
Felipe Lima <felipe.vlgiacomelli@gmail.com>
2018-11-06 18:24:52 -0200

    correção de texto e limpeza de arquivo .R

--- discursos.R
+++ discursos.R
@@ -1,9 +1,4 @@
-setwd("C:/Users/Felipe/Downloads/Curso/Discursos")
-install.packages("wordcloud2")
-install.packages("colorspace")
-install.packages("knitr")
-install.packages("htmlwidgets")
-install.packages("webshot")
+#loading packages
 
 library(knitr)
 library(magrittr)
@@ -26,78 +21,19 @@ library(webshot)
 
 webshot::install_phantomjs()
 
-?tidytext
+#loading Haddad's speech
 
 text <- readLines("Disc_Haddad.txt")
-docs <- Corpus(VectorSource(text))
 
-
-toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
-docs <- tm_map(docs, toSpace, "/")
-docs <- tm_map(docs, toSpace, "@")
-docs <- tm_map(docs, toSpace, "\\|")
-
-docs <- tm_map(docs, tolower)
-docs <- tm_map(docs, removeNumbers)
-docs <- tm_map(docs, removeWords, stopwords('portuguese'))
-docs <- tm_map(docs, removePunctuation)
-docs <- tm_map(docs, stripWhitespace)
-docs <- tm_map(docs, stemDocument)
-
-inspect(docs)
-
-#backup
-
-dtm <- TermDocumentMatrix(docs)
-m <- as.matrix(dtm)
-v <- sort(rowSums(m),decreasing=TRUE)
-d <- data.frame(word = enc2native(names(v)),freq=v)
-
-d$word <- as.character(d$word)
-
-set.seed(1234)
-wordcloud(enc2native(d$word), freq = d$freq, min.freq = 2, scale = c(3, 0.1), 
-          max.words=200, random.order=FALSE, rot.per=0, 
-          colors=brewer.pal(9, "Reds"))
-
-
-?wordcloud
-
-#stopwords
-
-#salvando rm
-rm(toSpace)
-rm(docs)
-rm(d)
-rm(dtm)
-rm(m)
-rm(v)
-
-#tentativa com rvest
-
-url <- 'https://www.poder360.com.br/eleicoes/leia-a-integra-do-discurso-de-derrota-fernando-haddad/'
-webpage <- read_html(url)
-
-#using CSS selectors to scrap the ranking section
-dh_html <- html_nodes(webpage, ".visible-xs~ p em , p:nth-child(13) em , p:nth-child(12) em , p:nth-child(11) em")
-dh <- html_text(dh_html)
-
-#backup dh
-dh2 <- dh
-
-#continuando
-
-text <- readLines("Disc_Haddad.txt")
+#create df from Haddad'speech
 
 dh_df <- tbl_df(map_df(text, as.data.frame))
 
+#tidying df
+
 dh_words <- dh_df %>% unnest_tokens(word, .x[[i]])
 
-#até aqui deu certo
-
-#backup de dh_words
-
-dh_words2 <- dh_words
+#cleaning df
 
 dh_words$word <- dh_words$word %>% 
   tolower %>% 
@@ -106,6 +42,8 @@ dh_words$word <- dh_words$word %>%
   removeWords(., stopwords('pt')) %>%
   trimws("l")
 
+# removing blank spaces and summarzing by word count
+
 dh_check <- dh_words %>%
   select(word) %>%
   group_by(word) %>%
@@ -113,16 +51,14 @@ dh_check <- dh_words %>%
   filter(total < 10) %>%
   arrange(desc(total)) 
 
-#ok até aqui
+#creating simple wordcloud
 
 set.seed(1234)
 wordcloud(dh_check$word, freq = dh_check$total, min.freq = 1, scale = c(3, 0.1), 
           max.words=200, random.order=FALSE, rot.per=0, 
           colors=brewer.pal(9, "Reds"))
 
-#ok
-
-#falta criar uma escala manual de cores com ao menos 18 passos
+#creating new wordcloud with manual palette
 
 wordcloud2(dh_check, size=.4, fontWeight = "600", 
            color = rev(brewer.pal(9, "Reds")), shape = 'circle')
@@ -134,18 +70,23 @@ hwc <- wordcloud2(dh_check, size=.4, fontWeight = "600",
                      "IndianRed", "IndianRed", "IndianRed", "IndianRed", "IndianRed", "IndianRed", "IndianRed", "IndianRed",
                      "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral", "LightCoral"), shape = 'circle')
 
+#using webshot to creat an image for the RMD file
+
 saveWidget(hwc,"wordcloud.html",selfcontained = F)
 webshot::webshot("wordcloud.html","wordcloud.png",vwidth = 800, vheight = 800, delay = 60)
 
+#doing the same with Bolsonaro's speech
 
-#agora com b
-
+#loading the data
 text_b <- readLines("Disc_B.txt")
 
+#creating df
 b_df <- tbl_df(map_df(text_b, as.data.frame))
 
+#tidying df
 b_words <- b_df %>% unnest_tokens(word, .x[[i]])
 
+#cleaning df
 b_words$word <- b_words$word %>% 
   tolower %>% 
   removePunctuation %>%
@@ -153,6 +94,7 @@ b_words$word <- b_words$word %>%
   removeWords(., stopwords('pt')) %>%
   trimws("l")
 
+#summarzing df by wordcount
 b_check <- b_words %>%
   select(word) %>%
   group_by(word) %>%
@@ -160,15 +102,16 @@ b_check <- b_words %>%
   filter(total < 10) %>%
   arrange(desc(total)) 
 
+#creating simple wordcloud
 set.seed(1234)
 wordcloud(b_check$word, freq = b_check$total, min.freq = 1, scale = c(3, 0.1), 
           max.words=70, random.order=FALSE, rot.per=0, 
           colors=brewer.pal(10, "BrBG"))
 
+#creating wordcloud2
 wordcloud2(b_check, size=.4, fontWeight = "600", 
            color = c("DarkGreen", "DarkOliveGreen", "OliveDrab", "OliveDrab",
                      "Green", "Green",
                      "SeaGreen", "SeaGreen", "SeaGreen", "SeaGreen", "SeaGreen", "SeaGreen", "SeaGreen", "SeaGreen", "SeaGreen", "SeaGreen", "SeaGreen", "SeaGreen", "SeaGreen", "SeaGreen", "SeaGreen",
                      "LightSeaGreen", "LightSeaGreen", "LightSeaGreen", "LightSeaGreen", "LightSeaGreen", "LightSeaGreen", "LightSeaGreen", "LightSeaGreen", "LightSeaGreen", "LightSeaGreen"),
                      shape = 'circle')
-
